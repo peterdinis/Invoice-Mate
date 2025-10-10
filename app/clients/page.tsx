0ef74195af680ac7f52341439867513e892a2dd4
peterdinis <petter1@pobox.sk>
2025-10-10 21:00:29 +0200

    feat: Clients page

---
+++ app/clients/page.tsx
@@ -0,0 +1,8 @@
+import ClientsWrapper from "@/components/clients/ClientsWrapper";
+import { NextPage } from "next";
+
+const ClientsPage: NextPage = () => {
+    return <ClientsWrapper />
+}
+
+export default ClientsPage
