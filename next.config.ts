8d2a4d79faa8e3d3f7ded0bd77a72a76eedd519e
peterdinis <petter1@pobox.sk>
2025-10-10 16:12:05 +0200

    infra: Initialize shadcn ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    typedEnv: true
+  },
+  typedRoutes: true
 };
 
 export default nextConfig;
