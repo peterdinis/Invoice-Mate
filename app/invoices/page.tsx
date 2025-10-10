0dd6ee1ee80eec7fbed4a0388668d98b4d53444d
peterdinis <petter1@pobox.sk>
2025-10-10 20:18:37 +0200

    feat: Design pages WIP

---
+++ app/invoices/page.tsx
@@ -0,0 +1,8 @@
+import InvoicesWrapper from "@/components/invoices/InvoicesWrapper";
+import { NextPage } from "next";
+
+const InvoicesPage: NextPage = () => {
+    return <InvoicesWrapper />
+}
+
+export default InvoicesPage
