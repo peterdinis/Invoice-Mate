73842795879632ef8166b69510a28a992979de79
peterdinis <petter1@pobox.sk>
2025-10-11 15:39:34 +0200

    infra: Add tanstack-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import ScrollToTop from "@/components/shared/ScrollToTop";
+import QueryProvider from "@/components/providers/QueryProvide";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,8 +36,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
-          <ScrollToTop />
+          <QueryProvider>
+            {children}
+            <ScrollToTop />
+          </QueryProvider>
         </ThemeProvider>
       </body>
     </html>
