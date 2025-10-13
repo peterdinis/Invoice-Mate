389e11201a43677a5e1882ff1516240998baf97b
peterdinis <petter1@pobox.sk>
2025-10-13 14:33:56 +0200

    feat: Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning suppressContentEditableWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
