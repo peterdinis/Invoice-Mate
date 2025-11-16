20ac922b67c126e56f4efa0743920ec2aca198aa
peterdinis <petter1@pobox.sk>
2025-11-16 10:47:07 +0100

    feat: Update ThemeProvider

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import * as React from "react";
+import { ComponentProps, useState, useEffect } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+}: ComponentProps<typeof NextThemesProvider>) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
